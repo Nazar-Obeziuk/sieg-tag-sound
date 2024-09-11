269833f9cdd5c175b2ebc61da4a0a1785f85dd0c
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-09-11 08:56:19 +0200

    sg

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -3,11 +3,7 @@ import axios from "axios";
 // connect to backend
 
 const instance = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:5555/",
-=======
   baseURL: "https://siegtagsound.com/api",
->>>>>>> a8a7489da7bf2ca980d176b5afd2fd9610692fa7
 });
 
 export default instance;
